f23b6b8aaa50918875e4d3047731e57462757cba
Reza15060 <shourovreza@gmail.com>
2022-02-07 02:09:41 +0600

    deployed

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Charts from "../../components/chart/Charts";
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
+import WidgetSm from "../../components/wigetSm/WidgetSm";
+import { userData } from "../../dummyData";
+
+
+import "./home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <FeaturedInfo />
+      <Charts
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
